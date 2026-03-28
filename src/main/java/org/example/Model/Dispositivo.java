bba53e0d95d2122e032249a5e0749caca1908408
Luizsellitto <luizsellitto2609@gmail.com>
2026-03-28 18:26:29 -0300

    Feat: Classe Dispositivo

---
+++ src/main/java/org/example/Model/Dispositivo.java
@@ -0,0 +1,47 @@
+package org.example.Model;
+
+public class Dispositivo {
+    private Integer id;
+    private String nome;
+    private int status;
+    private String tipo;
+
+    public Dispositivo(String nome, String tipo) {
+        this.nome = nome;
+        this.status = 0;
+        this.tipo = tipo;
+    }
+    public Dispositivo(int id, String nome, int status, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.status = status;
+        this.tipo = tipo;
+    }
+
+    public void ligar() { status = 1; }
+    public void desligar() { status = 0; }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
