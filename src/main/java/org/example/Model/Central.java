7ba85f38096f1a3e3e4b2d5a24924c07cdcf92b4
Luizsellitto <luizsellitto2609@gmail.com>
2026-03-29 15:42:34 -0300

    Refactor: Adequando a Central para os DAOs

--- src/main/java/org/example/Model/Central.java
+++ src/main/java/org/example/Model/Central.java
@@ -1,18 +1,22 @@
 package org.example.Model;
 
 import org.example.DAO.DispositivoDAO;
+import org.example.DAO.LampadaDAO;
 
 import java.util.*;
 
 public class Central {
 
     private static Central instance;
-    private Map<Integer, Dispositivo> dispositivos = new HashMap<>();
-    private DispositivoDAO dao;
-    private boolean carregado = false;
+
+    private final Map<Integer, Dispositivo> dispositivos = new HashMap<>();
+
+    private final DispositivoDAO dispositivoDAO;
+    private final LampadaDAO lampadaDAO;
 
     private Central() throws Exception {
-        dao = new DispositivoDAO();
+        dispositivoDAO = new DispositivoDAO();
+        lampadaDAO = new LampadaDAO();
         carregarDoBanco();
     }
 
@@ -24,31 +28,54 @@ public class Central {
     }
 
     private void carregarDoBanco() throws Exception {
-        if (carregado) return;
+        dispositivos.clear();
 
-        for (Dispositivo d : dao.getAll()) {
-            dispositivos.put(d.getId(), d);
+        for (Dispositivo d : dispositivoDAO.getAll()) {
+
+            if (d.getTipo().equalsIgnoreCase("lampada")) {
+                Lampada lampada = lampadaDAO.getById(d.getId());
+                dispositivos.put(d.getId(), lampada);
+            } else {
+                dispositivos.put(d.getId(), d);
+            }
         }
-
-        carregado = true;
     }
 
     public void salvar(Dispositivo d) throws Exception {
-        dao.insert(d);
-        dispositivos.put(d.getId(), d);
+        dispositivoDAO.insert(d);
+
+        if (d instanceof Lampada lampada) {
+            lampadaDAO.insert(lampada);
+        }
+
+        carregarDoBanco();
     }
 
     public void atualizar(Dispositivo d) throws Exception {
-        dao.update(d);
-        dispositivos.put(d.getId(), d);
+        dispositivoDAO.update(d);
+
+        if (d instanceof Lampada lampada) {
+            lampadaDAO.update(lampada);
+        }
+
+        carregarDoBanco();
     }
 
     public void remover(int id) throws Exception {
         Dispositivo d = dispositivos.get(id);
+
         if (d != null) {
-            dao.delete(d);
-            dispositivos.remove(id);
+            if (d instanceof Lampada) {
+                lampadaDAO.delete(id);
+            }
+
+            dispositivoDAO.delete(d);
+            carregarDoBanco();
         }
+    }
+
+    public Dispositivo getById(int id) {
+        return dispositivos.get(id);
     }
 
     public Collection<Dispositivo> getDispositivos() {
