4093d96911f270c23a745be1420fa7019f692dbf
Luizsellitto <luizsellitto2609@gmail.com>
2026-03-29 14:44:58 -0300

    Refactor: DatabaseInitializer ativando chave estrangeira e criando tabela lampada

--- src/main/java/org/example/DatabaseInitializer.java
+++ src/main/java/org/example/DatabaseInitializer.java
@@ -10,13 +10,23 @@ public class DatabaseInitializer {
         try (Connection conn = ConnectionFactory.getConnection();
              Statement st = conn.createStatement()) {
 
+            st.execute("PRAGMA foreign_keys = ON");
+
             st.execute("""
                 CREATE TABLE IF NOT EXISTS dispositivo (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     nome TEXT,
                     status INTEGER,
                     tipo TEXT
-                );
+                )
+            """);
+
+            st.execute("""
+                CREATE TABLE IF NOT EXISTS lampada (
+                    id INTEGER PRIMARY KEY,
+                    intensidade INTEGER,
+                    FOREIGN KEY (id) REFERENCES dispositivo(id)
+                )
             """);
         }
     }
